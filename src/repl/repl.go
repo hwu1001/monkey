b08b09f85d6c967ba062cbcdc72bc70078e53c8a
Henry Wu <hywu925@gmail.com>
2020-06-17 21:39:18 -0700

    Add the E in REPL, but only for int expressions

--- src/repl/repl.go
+++ src/repl/repl.go
@@ -5,6 +5,7 @@ import (
 	"fmt"
 	"io"
 
+	"github.com/v1zix/monkey/src/evaluator"
 	"github.com/v1zix/monkey/src/lexer"
 	"github.com/v1zix/monkey/src/parser"
 )
@@ -43,8 +44,11 @@ func Start(in io.Reader, out io.Writer) {
 			continue
 		}
 
-		io.WriteString(out, program.String())
-		io.WriteString(out, "\n")
+		evaluated := evaluator.Eval(program)
+		if evaluated != nil {
+			io.WriteString(out, evaluated.Inspect())
+			io.WriteString(out, "\n")
+		}
 	}
 }
 
