8b807ffd7d1cfa2e905dd978b51e518ed8659f40
Henry Wu <hywu925@gmail.com>
2021-01-19 23:39:04 -0800

    Create env, eval let and identifiers

--- src/repl/repl.go
+++ src/repl/repl.go
@@ -7,6 +7,7 @@ import (
 
 	"github.com/v1zix/monkey/src/evaluator"
 	"github.com/v1zix/monkey/src/lexer"
+	"github.com/v1zix/monkey/src/object"
 	"github.com/v1zix/monkey/src/parser"
 )
 
@@ -26,6 +27,7 @@ const MONKEY_FACE = `            __,__
 
 func Start(in io.Reader, out io.Writer) {
 	scanner := bufio.NewScanner(in)
+	env := object.NewEnvironment()
 
 	for {
 		fmt.Fprintf(out, PROMPT)
@@ -44,7 +46,7 @@ func Start(in io.Reader, out io.Writer) {
 			continue
 		}
 
-		evaluated := evaluator.Eval(program)
+		evaluated := evaluator.Eval(program, env)
 		if evaluated != nil {
 			io.WriteString(out, evaluated.Inspect())
 			io.WriteString(out, "\n")
