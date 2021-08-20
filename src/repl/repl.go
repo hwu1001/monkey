99389831ce00770d532503c236086f3226ad9661
Henry Wu <hywu925@gmail.com>
2021-08-19 22:45:41 -0700

    Change repl to use compiler

--- src/repl/repl.go
+++ src/repl/repl.go
@@ -5,10 +5,10 @@ import (
 	"fmt"
 	"io"
 
-	"github.com/v1zix/monkey/src/evaluator"
+	"github.com/v1zix/monkey/src/compiler"
 	"github.com/v1zix/monkey/src/lexer"
-	"github.com/v1zix/monkey/src/object"
 	"github.com/v1zix/monkey/src/parser"
+	"github.com/v1zix/monkey/src/vm"
 )
 
 const PROMPT = ">> "
@@ -27,7 +27,6 @@ const MONKEY_FACE = `            __,__
 
 func Start(in io.Reader, out io.Writer) {
 	scanner := bufio.NewScanner(in)
-	env := object.NewEnvironment()
 
 	for {
 		fmt.Fprintf(out, PROMPT)
@@ -46,13 +45,55 @@ func Start(in io.Reader, out io.Writer) {
 			continue
 		}
 
-		evaluated := evaluator.Eval(program, env)
-		if evaluated != nil {
-			io.WriteString(out, evaluated.Inspect())
-			io.WriteString(out, "\n")
+		comp := compiler.New()
+		err := comp.Compile(program)
+		if err != nil {
+			fmt.Fprintf(out, "Woops! Compilation failed:\n %s\n", err)
+			continue
 		}
+
+		machine := vm.New(comp.Bytecode())
+		err = machine.Run()
+		if err != nil {
+			fmt.Fprintf(out, "Woops! Executing bytecode failed:\n %s\n", err)
+			continue
+		}
+
+		stackTop := machine.StackTop()
+		io.WriteString(out, stackTop.Inspect())
+		io.WriteString(out, "\n")
 	}
 }
+
+// Evaluator REPL
+// func Start(in io.Reader, out io.Writer) {
+// 	scanner := bufio.NewScanner(in)
+// 	env := object.NewEnvironment()
+
+// 	for {
+// 		fmt.Fprintf(out, PROMPT)
+// 		scanned := scanner.Scan()
+// 		if !scanned {
+// 			return
+// 		}
+
+// 		line := scanner.Text()
+// 		l := lexer.New(line)
+// 		p := parser.New(l)
+
+// 		program := p.ParseProgram()
+// 		if len(p.Errors()) != 0 {
+// 			printParserErrors(out, p.Errors())
+// 			continue
+// 		}
+
+// 		evaluated := evaluator.Eval(program, env)
+// 		if evaluated != nil {
+// 			io.WriteString(out, evaluated.Inspect())
+// 			io.WriteString(out, "\n")
+// 		}
+// 	}
+// }
 
 func printParserErrors(out io.Writer, errors []string) {
 	io.WriteString(out, MONKEY_FACE)
