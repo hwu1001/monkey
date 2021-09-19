bb9f6bebfaf31169fce7a5ad1e5cb84b80b74f00
Henry Wu <hywu925@gmail.com>
2021-09-19 00:17:14 -0700

    Support conditionals in vm

--- src/vm/vm.go
+++ src/vm/vm.go
@@ -85,6 +85,17 @@ func (vm *VM) Run() error {
 			if err != nil {
 				return err
 			}
+		case code.OpJump:
+			pos := int(code.ReadUint16((vm.instructions[ip+1:])))
+			ip = pos - 1
+		case code.OpJumpNotTruthy:
+			pos := int(code.ReadUint16((vm.instructions[ip+1:])))
+			ip += 2 // skip over 2 bytes of operand in next cycle
+
+			condition := vm.pop()
+			if !isTruthy(condition) {
+				ip = pos - 1
+			}
 		case code.OpPop:
 			vm.pop()
 		}
@@ -200,6 +211,15 @@ func nativeBoolToBooleanObject(input bool) *object.Boolean {
 		return True
 	}
 	return False
+}
+
+func isTruthy(obj object.Object) bool {
+	switch obj := obj.(type) {
+	case *object.Boolean:
+		return obj.Value
+	default:
+		return true
+	}
 }
 
 func (vm *VM) push(o object.Object) error {
